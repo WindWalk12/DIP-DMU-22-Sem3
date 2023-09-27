a3823611799492bd804626e96726a821b32abe23
WindWalk12 <nhygta@hotmail.com>
2023-09-27 19:18:22 +0200

    Done

--- L10-AsyncFunctions/Exercise2/opgave10.2.js
+++ L10-AsyncFunctions/Exercise2/opgave10.2.js
@@ -3,7 +3,7 @@ const userUrl = 'https://jsonplaceholder.typicode.com/users';
 const postUrl = 'https://jsonplaceholder.typicode.com/posts?userId=';
 
 function createNode(element) {
-    return document.createElement(element)
+    return document.createElement(element);
   }
 
 async function get(url) {
@@ -13,19 +13,41 @@ async function get(url) {
     return await respons.json();
 }
 
+let addPosts = async (id, name) => {
+    let posts = await get(postUrl + id);
+    let postsTBody = document.querySelector('#postsBody');
+    postsTBody.innerHTML = "";
+    let tr = createNode('tr');
+    postsTBody.append(tr);
+    let tdName = createNode('td');
+    tdName.innerHTML = `Posts from: ${name}`;
+    tr.append(tdName);
+    posts.forEach(posts => {
+        let tr = createNode('tr');
+        postsTBody.append(tr);
+        let tdTitle = createNode('td');
+        tdTitle.innerHTML = `Title: ${posts.title}`;
+        tr.append(tdTitle);
+        let tdBody = createNode('td');
+        tdBody.innerHTML = `Body: ${posts.body}`;
+        tr.append(tdBody);
+    })
+}
+
 let addUsers = async () => {
     let users = await get(userUrl);
-    let nameTHead = document.querySelector('#nameTHead');
     let infoTBody = document.querySelector('#infoTBody');
     users.forEach(user => {
-        let th = createNode('th');
-        th.innerHTML = user.name;
-        nameTHead.append(th);
         let tr = createNode('tr');
+        tr.addEventListener('click', () => {
+            addPosts(user.id, user.name);
+        });
         infoTBody.append(tr);
+        let td = createNode('td');
+        td.innerHTML = `Name: ${user.name} Id: ${user.id} Username: ${user.username} Phone: ${user.phone}`;
+        tr.append(td);
         
     });
-    console.log(users);
 }
 
 addUsers();
